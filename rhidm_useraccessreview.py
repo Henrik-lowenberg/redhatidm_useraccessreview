32d8305622930b68339953cf6a575fff074ca9f4
Henrik L <henrik.lowenberg@gmail.com>
2021-02-13 13:26:14 +0100

    update with file output

--- rhidm_useraccessreview.py
+++ rhidm_useraccessreview.py
@@ -14,12 +14,22 @@
 from python_freeipa import ClientMeta
 # pretty print to parse to human-readable output
 import pprint
-# parse json data
-import json
+# prepare outfile for excel import
+import csv
  
 # get login credentials
-client = ClientMeta("idmreplica1.idm.example.com")
-client.login("api-reader","apipwd")
+client = ClientMeta("segotl6204.idm.it.hclgss.com")
+client.login("api-reader","api12345")
+ 
+# Function to convert list to string
+def listToString(s):
+    # initialize an empty string
+    str1 = ""
+    # traverse in the string
+    for ele in s:
+        str1 += ele + ","
+    # return string
+    return str1
 
 # Get all IDM users
 # use the freeipa module user_find method and store in userlist list
@@ -37,24 +47,24 @@ for row in userresult:
     user = []
     user.append(row.get('uid')[0])
     user.append(row.get('cn')[0])
-    user.append(row.get('memberof_group'))
+    user.append(listToString(row.get('memberof_group')))
     # add user list to bigger list of all users
     listofusers.append(user)
  
-pretty_print = pprint.PrettyPrinter(indent=10,width=80,depth=None)
-for row in listofusers:
-#    print(row)
+#pretty_print = pprint.PrettyPrinter(indent=10,width=80,depth=None)
+#for row in listofusers:
 #    print(json.dumps(row))
-    pretty_print.pprint(json.dumps(row))
+#    pretty_print.pprint(json.dumps(row))
  
-print "\n"
-
-# write to file for later import to excel
+#print "\n"
+ 
 header_user = ['USER NAME', 'FULL NAME', 'GROUPS']
+ 
 with open('listofusers.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile)
+    writer = csv.writer(csvfile, delimiter=';')
     writer.writerow(header_user)
     writer.writerows(listofusers)
+ 
 
 # Get all user groups
 #grouplist = client.group_find(posix=True)
@@ -73,12 +83,17 @@ for row in groupresult:
     group.append(row.get('memberof_hbacrule'))
     listofgroups.append(group)
  
-for row in listofgroups:
-#    print(row)
-    print(json.dumps(row))
- 
-print "\n"
- 
+#for row in listofgroups:
+#    print(json.dumps(row))
+
+#print "\n"
+
+header_groups = ['GROUP NAME', 'GROUP MEMBERSHIP', 'SUDORULE', 'HBACRULE']
+with open('listofgroups.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(header_groups)
+    writer.writerows(listofgroups)
+
 # Get all host groups
 hostgrouplist = client.hostgroup_find()
 #print hostgrouplist.items()
@@ -92,12 +107,16 @@ for row in hostgroupresult:
     hostgroup.append(row.get('member_host'))
     listofhostgroups.append(hostgroup)
  
-for row in listofhostgroups:
-#    print(row)
-    print(json.dumps(row))
+#for row in listofhostgroups:
+#    print(json.dumps(row))
  
-print "\n"
+#print "\n"
 
+header_hostgroups = ['HOSTGROUP NAME', 'HBACRULE MEMBERSHIP', 'HOSTS']
+with open('listofhostgroups.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(header_hostgroups)
+    writer.writerows(listofhostgroups)
  
 # get all HBAC rules
 hbacrulelist = client.hbacrule_find()
@@ -112,12 +131,17 @@ for row in hbacruleresult:
     hbacrule.append(row.get('accessruletype'))
     hbacrule.append(row.get('memberuser_group'))
     listofhbacrules.append(hbacrule)
+
+#for row in listofhbacrules:
+#    print(json.dumps(row))
+
+#print "\n"
  
-for row in listofhbacrules:
-#    print(row)
-    print(json.dumps(row))
- 
-print "\n"
+header_hbacrules = ['HBACRULE NAME', 'MEMBER USER', 'TYPE OF ACCESS', 'MEMBER GROUP']
+with open('listofbacrules.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(header_hbacrules)
+    writer.writerows(listofhbacrules)
  
 # get all Sudo rules
 sudorulelist = client.sudorule_find()
@@ -135,11 +159,17 @@ for row in sudoruleresult:
     sudorule.append(row.get('cmdcategory'))
     listofsudorules.append(sudorule)
  
-for row in listofsudorules:
+#for row in listofsudorules:
 #    print(row)
-    print(json.dumps(row))
+#    print(json.dumps(row))
+
+#print "\n"
  
-print "\n"
+header_sudorules = ['SUDORULE NAME', 'RUN AS', 'MEMBER HOST', 'MEMBER USERGROUP', 'MEMBER HOSTGROUP', 'CMD CATEGORY']
+with open('listofsudorules.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(header_sudorules)
+    writer.writerows(listofsudorules)
  
 # get all Sudo commands
 sudocmdlist = client.sudocmd_find()
@@ -147,37 +177,53 @@ sudocmdlist = client.sudocmd_find()
 sudocmdresult = sudocmdlist.get("result")
 listofsudocmds = []
 listofsudocmds.append(['SUDOCMD NAME', 'DESCRIPTION'])
+
 for row in sudocmdresult:
     sudocmd = []
     sudocmd.append(row.get('sudocmd')[0])
     sudocmd.append(row.get('description'))
     listofsudocmds.append(sudocmd)
+
+#for row in listofsudocmds:
+#    print(json.dumps(row))
  
-for row in listofsudocmds:
-#    print(row)
-    print(json.dumps(row))
- 
-print "\n"
+#print "\n"
+
+
+header_sudocmds = ['SUDOCMD NAME', 'DESCRIPTION']
+with open('listofsudocmds.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(header_sudocmds)
+    writer.writerows(listofsudocmds)
  
 # get all Sudo command groups
 sudocmdgrouplist = client.sudocmdgroup_find()
 #print sudocmdgrouplist
 sudocmdresult = sudocmdgrouplist.get("result")
-#type(sudocmdresult)
 listofsudocmdgroups = []
 listofsudocmdgroups.append(['SUDOCMDGROUP NAME', 'SUDO CMD'])
-
 for row in sudocmdresult:
     sudocmdgroup = []
-#    sudocmdgroup.append(row.get([0]))
     sudocmdgroup.append(row.get('cn'))
     sudocmdgroup.append(row.get('member_sudocmd'))
     listofsudocmdgroups.append(sudocmdgroup)
+
+#for row in listofsudocmdgroups:
+#    print(json.dumps(row))
+#print "\n"
+sarss, 
  
-for row in listofsudocmdgroups:
-#    print(row)
-    print(json.dumps(row))
+
+header_sudocmdgroups = ['SUDOCMDGROUP NAME', 'SUDO CMD']
+
+with open('listofsudocmdgroups.csv', 'wb') as csvfile:
+
+    writer = csv.writer(csvfile, delimiter=';')
+
+    writer.writerow(header_sudocmdgroups)
+
+    writer.writerows(listofsudocmdgroups)
+
  
-print "\n"
 
 # END OF SCRIPT #
